6e4e28599f1f67207353c8dd16be23de9541acac
pyphil <63059088+pyphil@users.noreply.github.com>
2024-09-28 23:53:44 +0200

    activity form

--- activity/models.py
+++ activity/models.py
@@ -1,5 +1,28 @@
 from django.db import models
 
 
+class ActivityBlock(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Activity(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Activities'
+
+
+class BookedActivity(models.Model):
+    block = models.ForeignKey(ActivityBlock, on_delete=models.PROTECT)
+    activity = models.ForeignKey(Activity, on_delete=models.PROTECT)
     student_name = models.CharField(max_length=200)
+    parents_name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'Booked Activities'
--- activity/views.py
+++ activity/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
-from .models import Activity
-from .forms import ActivityForm
+from django.shortcuts import render, redirect
+from .models import Activity, ActivityBlock, BookedActivity
 
 
-def activity(request):
-    f = ActivityForm()
-    return render(request, 'activity.html', {'form': f})
+def activity(request):    
+    blocks = ActivityBlock.objects.all()
+    current_block_id = int(request.GET.get('block', blocks.first().id))
+    alert = None
+    if request.method == 'POST':
+        selected_block = ActivityBlock.objects.get(id=current_block_id)
+        selected_activity = Activity.objects.get(name=request.POST.get('select_activity'))
+        student_name = request.POST.get('student_name').strip()
+        parents_name = request.POST.get('parents_name').strip()
+        count = BookedActivity.objects.filter(block=selected_block, activity=selected_activity).count()
+        if count <= 5:
+            BookedActivity.objects.create(
+                block=selected_block,
+                activity=selected_activity,
+                student_name=student_name,
+                parents_name=parents_name
+            ).full_clean()
+            return redirect('activity')
+        else:
+            alert = selected_activity
+
+    activities = Activity.objects.all()
+    activity_list = []
+    for activity in activities:
+        count = BookedActivity.objects.filter(block=current_block_id, activity=activity).count()
+        if count > 5:
+            activity_list.append(activity.name + ' - ausgebucht')
+        else:
+            activity_list.append(activity.name)
+
+    return render(request, 'activity.html', {
+        'activities': activity_list,
+        'blocks': blocks,
+        'current_block_id': current_block_id,
+        'alert': alert
+        }
+    )
