c2b9a86b69029752c081b973ec6e48dd5c3a402e
pyphil <pyphil@users.noreply.github.com>
2025-03-08 12:30:49 +0100

    add userprofile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import RegistrationID
+from .models import RegistrationID, UserProfile
 
 
 admin.site.register(RegistrationID)
+admin.site.register(UserProfile)
--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    abbr = models.CharField(max_length=3)
+    email = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.PROTECT, null=True, blank=True)
+
+    def __str__(self):
+        return self.abbr
 
 
 class RegistrationID(models.Model):
