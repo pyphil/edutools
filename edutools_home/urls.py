3aef3dc5021e566fd21ffb24b8b2b43fd3625064
pyphil <pyphil@users.noreply.github.com>
2025-03-27 17:22:59 +0100

    add user profiles management views and templates

--- edutools_home/urls.py
+++ edutools_home/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.edutools_home, name='edutools_home'),
     path('dsb', views.dsb, name='dsb'),
+    path('userprofiles/', views.userprofiles, name='userprofiles'),
+    path('userprofile_details/<int:id>/', views.userprofile_details, name='userprofile_details'),
     #path('mkr', views.mkr, name='mkr'),
 ]
--- edutools_home/views.py
+++ edutools_home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from buchungstool_settings.models import Setting
+from django.contrib.admin.views.decorators import staff_member_required
+from accounts.models import UserProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 
 
 def edutools_home(request):
@@ -11,6 +15,37 @@ def edutools_home(request):
 def dsb(request):
     settings = Setting.objects.filter(name='settings').first()
     return redirect(settings.dsb_link)
+
+
+@login_required
+@staff_member_required
+def userprofiles(request):
+    userprofiles = UserProfile.objects.all()
+    return render(request, "edutools_userprofiles.html", {"userprofiles": userprofiles})
+
+
+@login_required
+@staff_member_required
+def userprofile_details(request, id):
+    userprofile = UserProfile.objects.get(id=id)
+    groups = Group.objects.all()
+    if request.method == "POST":
+        userprofile.abbr = request.POST.get("abbr")
+        if userprofile.user:
+            userprofile.user.groups.set(request.POST.getlist("groups"))
+            user = userprofile.user
+            if request.POST.get("is_staff"):
+                user.is_staff = True
+            else:
+                user.is_staff = False
+            if request.POST.get("is_active"):
+                user.is_active = True
+            else:    
+                user.is_active = False
+            user.save()
+        userprofile.save()
+        return redirect("userprofiles")
+    return render(request, "edutools_userprofile_details.html", {"userprofile": userprofile, "groups": groups})
 
 
 # def mkr(request):
