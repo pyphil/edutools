fd289d803e1a678212d0f81d5d0e7918c6210804
pyphil <63059088+pyphil@users.noreply.github.com>
2025-05-12 15:19:20 +0200

    add import user profiles from CSV functionality and corresponding template

--- edutools_home/urls.py
+++ edutools_home/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('userprofiles/', views.userprofiles, name='userprofiles'),
     path('userprofile_details/<int:id>/', views.userprofile_details, name='userprofile_details'),
     path('userprofile_add/', views.userprofile_add, name='userprofile_add'),
+    path('import_userprofiles/', views.import_userprofiles_from_csv, name='import_userprofiles'),
     #path('mkr', views.mkr, name='mkr'),
 ]
--- edutools_home/views.py
+++ edutools_home/views.py
@@ -5,15 +5,17 @@ from django.contrib.admin.views.decorators import staff_member_required
 from accounts.models import UserProfile
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
+import csv
+from django.contrib import messages
 
 
 def edutools_home(request):
-    return render(request, 'edutools_home.html', {})
+    return render(request, "edutools_home.html", {})
 
 
 @login_required
 def dsb(request):
-    settings = Setting.objects.filter(name='settings').first()
+    settings = Setting.objects.filter(name="settings").first()
     return redirect(settings.dsb_link)
 
 
@@ -31,6 +33,7 @@ def userprofile_details(request, id):
     groups = Group.objects.all()
     if request.method == "POST":
         userprofile.abbr = request.POST.get("abbr")
+        userprofile.email = request.POST.get("email")
         if userprofile.user:
             userprofile.user.groups.set(request.POST.getlist("groups"))
             user = userprofile.user
@@ -45,7 +48,11 @@ def userprofile_details(request, id):
             user.save()
         userprofile.save()
         return redirect("userprofiles")
-    return render(request, "edutools_userprofile_details.html", {"userprofile": userprofile, "groups": groups})
+    return render(
+        request,
+        "edutools_userprofile_details.html",
+        {"userprofile": userprofile, "groups": groups},
+    )
 
 
 @login_required
@@ -71,7 +78,47 @@ def userprofile_add(request):
             user.save()
         userprofile.save()
         return redirect("userprofiles")
-    return render(request, "edutools_userprofile_details.html", {"userprofile": userprofile, "groups": groups})
+    return render(
+        request,
+        "edutools_userprofile_details.html",
+        {"userprofile": userprofile, "groups": groups},
+    )
+
+
+@login_required
+@staff_member_required
+def import_userprofiles_from_csv(request):
+    if request.method == "POST" and request.FILES.get("csv_file"):
+        csv_file = request.FILES["csv_file"]
+        if not csv_file.name.endswith(".csv"):
+            messages.error(request, "Please upload a valid CSV file.")
+            return redirect("import_userprofiles")
+
+        try:
+            decoded_file = csv_file.read().decode("utf-8").splitlines()
+            reader = csv.DictReader(decoded_file)
+            for row in reader:
+                abbr = row.get("Krzl")
+                email = row.get("email")
+                if abbr and email:
+                    userprofile, created = UserProfile.objects.get_or_create(
+                        email=email
+                    )
+                    userprofile.abbr = abbr
+                    userprofile.save()
+                else:
+                    messages.error(
+                        request,
+                        f"An error occurred: Make sure there are 'Krzl' and 'email' columns.",
+                    )
+                    return redirect("import_userprofiles")
+            messages.success(request, "UserProfiles imported successfully.")
+        except Exception as e:
+            messages.error(request, f"An error occurred: {str(e)}")
+            return redirect("import_userprofiles")
+        return redirect("userprofiles")
+
+    return render(request, "edutools_import_userprofiles.html")
 
 
 # def mkr(request):
