1bfd77eb7ab3e84f7e4339e5159a7b6373006aed
pyphil <pyphil@users.noreply.github.com>
2025-05-22 19:52:52 +0200

    Register UserProfile with custom admin interface for improved display and filtering

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import RegistrationID, UserProfile
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+    # fields to display in the listing
+    list_display = (
+        "abbr",
+        "email",
+        "user",
+    )
+
+    # enable results filtering
+    list_filter = (
+        "abbr",
+        "user",
+    )
+
+    # number of items per page
+    # list_per_page = 25
+    list_max_show_all = 2000
+
+    # Default results ordering
+    ordering = ["-abbr"]
+
+
 admin.site.register(RegistrationID)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
